d4548387bdd628055513aed54ffeeebf0ece3241
kennpat <kennedypr@gmail.com>
2014-11-20 17:06:03 -0500

    Commit working first half of the assignment, now lists positions with links

--- server.rb
+++ server.rb
@@ -1,3 +1,7 @@
+#Patrick Kennedy & Nick Clucas
+#Kickball Challenge
+#11/18/14
+
 require "sinatra"
 require "json"
 require "pry"
@@ -8,23 +12,36 @@ team_file = File.read('roster.json')
 teams = JSON.parse(team_file)
 
 team_name_hash = {}
-get '/' do 
-	@teams = team_name_hash
-	erb :index
-end
+position_arr = []
+
+position_arr = teams["Simpson Slammers"].keys
 
 teams.keys.each do |team|
 	team_name_hash[team] = team.downcase.split(' ').join('_')
 end
 
-get '/*' do |link|
+get '/' do 
+	@teams = team_name_hash
+	@positions = position_arr
+	binding.pry
+	erb :index
+end
+
+get '/teams/*' do |link|
 	team_name_hash.values
 	@team_name = teams.keys[team_name_hash.values.index(link)]
 	@team_roster = teams.values[team_name_hash.values.index(link)]
 	erb :teams
 end
 
+get '/positions/*' do |link|
+	position_arr
+	@postion = ''		
+	@player = ''
+	erb :positions
+end
 
+#binding.pry
 
 
 
